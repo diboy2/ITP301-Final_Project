66a2d06875b654eb8057d30144529554825e3ff4
Adrian <cagaanan@usc.edu>
2015-04-13 14:14:47 -0700

    Added Module

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name itp301FinalProjectApp
+ * @description
+ * # itp301FinalProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('itp301FinalProjectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap'
+  ]);
